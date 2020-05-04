12314bcdc5d892b827e0c5facc000235b9478561
Berat Burak TETİK <tetik_640@hotmail.com>
2020-05-04 15:29:28 +0300

    HW4

---
+++ CetBookStore/ViewComponents/SearchCategoryViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using CetBookStore.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CetBookStore.ViewComponents
+{
+    public class SearchCategoryViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext context;
+
+        public SearchCategoryViewComponent(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var cats = await context.Categories.ToListAsync();
+            return View(cats);
+        }
+    }
+}
